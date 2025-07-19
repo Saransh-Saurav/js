ab23a35f464fe06bf750312f95b3533940a9545c
Saransh-Saurav <saranshcollegebackup@gmail.com>
2025-07-19 14:56:19 +0000

    done

---
+++ 01-basics/02_comparision.js
@@ -0,0 +1,26 @@
+// console.log(null>4);
+// console.log(null==4);
+// console.log(null>=4);
+// //all of them gives false as null is conversted to 0 
+// console.log(null<=4);//true
+
+
+// console.log(null>0);//false 
+// console.log(null==0);//false
+// console.log(null>=0);//true
+// console.log(null<=0);//true
+//using comparision operator(<,>,<=,>=)null is converted to 0
+//but using the equality operator(==),it actually see if its equal or not thats whay it gives false
+
+
+
+// console.log(undefined>0);//false 
+// console.log(undefined==0);//false
+// console.log(undefined>=0);//false
+// console.log(undefined<=0);//false
+//undefined doesnot get converted so it gives false
+
+// let p=Number(undefined);
+// console.log(p);//NaN
+// console.log("2"===2);//false //strict check,check data type also
+
