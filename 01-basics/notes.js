a294188d48b41421611431733a23197b2cd1174a
Saransh-Saurav <saranshcollegebackup@gmail.com>
2025-07-19 14:37:33 +0000

    done

--- 01-basics/notes.js
+++ 01-basics/notes.js
@@ -4,4 +4,33 @@ let Typeofvar= Number(n);
 
 let p=45;
 let Typeofvariable= String(p);
-console.log(Typeofvariable);
+// console.log(Typeofvariable);
+
+
+let value=7;
+let negvalue=-value;
+// console.log(negvalue);
+
+
+// console.log(2/3);
+// console.log(2&3);
+// let str1="ho"
+// let str2="ho"
+
+// let str3=str1+str2
+// // console.log(str3);
+
+// let str4=str1-str2
+// console.log(str4);
+
+// let str5=str1*str2
+// console.log(str5);
+
+
+//  console.log(+true);
+//  console.log(-true);
+//  console.log(+"he");
+ 
+
+
+
