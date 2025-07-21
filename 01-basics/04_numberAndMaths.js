78488482edf488657b1c6717cd3e09389e2baf28
Saransh-Saurav <saranshcollegebackup@gmail.com>
2025-07-21 17:14:08 +0000

    k

---
+++ 01-basics/04_numberAndMaths.js
@@ -0,0 +1,18 @@
+const score=900;
+console.log(score);//900
+
+const marks =new Number(987);//basically typecasting to number
+console.log(marks);//[Number: 987]
+
+const n =marks.toString();//number converted to string
+console.log(n[1]);//8
+console.log(n.length);//3 -->basically string properties can be used for it now
+
+console.log(marks.toFixed(2));//987.00
+//USED FOR NUMBERS --->toFixed round off the number till the decimal place we specify inside ()
+
+
+
+
+
+
