c8711ffd1c3088357962bd8d4efe64cd879e9e83
Saransh-Saurav <saranshcollegebackup@gmail.com>
2025-07-21 12:15:49 +0000

    new stuffs

--- 01-basics/03_strings.js
+++ 01-basics/03_strings.js
@@ -1,3 +1,5 @@
+//CHECK OUT MDN OF FUNCTION TO GAIN MORE KNOWLEDGE
+
 let name="Saransh"
 const age=18;
 // console.log("name is", name ,"age is", age );  //this is onle way to print both string and some message
@@ -5,7 +7,7 @@ const age=18;
 
 // console.log(`name is ${name} age is ${age}`);  //this would be used more as we can perform some operation inside the {} also and remenber t is `` and not ''
 
-// console.log(name[0]);
+// console.log(name[0]);//S
 
 
 
@@ -55,8 +57,56 @@ const n=new String("sara");
 // console.log(n.charAt(2)); //r //tells the charcter which is at index 2
 // console.log(n.indexOf("s")); //0  //tells the first occourance of given char at the index
 
-const newstring = n.substring(0,5);//USED FOR SLICING
-console.log(newstring);//sara
+
+
+
+
+
+
+
+
+const p=new String("hello_world");
+
+const newstring = p.substring(0,7);//USED FOR SLICING BUT DOESNT INCLUDE -VE SLICING
+//REMEMBER IT (0,7)MEANS START FROM INDEX 0 AND GO UPTO INDEX 7-1=6 VALUES i.e INDEX 0,1,2,3,4,5,6
+console.log(newstring);//hello_w
+
+//ANOTHER WAY OF SLICING(IT INCLUDES NEGATIVE SLICING)
+let anotherstring=p.slice(0,5);
+console.log(anotherstring);//hello
+
+anotherstring=p.slice(-7,-1);
+console.log(anotherstring)//o_worl  -->it started with index -7("o") and goes till index -2("l")
+anotherstring=p.slice(-10,6);
+console.log(anotherstring);//ello_ -->it started with index -10("e")and goes till index 5("_")
+//hello_world->here there are 2 ways of indexing 
+//1st 0index on "h" then it increases to 10index on "d"
+//2nd -1index on "d" then it decreases to -11index on "h"
+//remember it only moves forward i.e from left to right only even if we type things like(-7,-11) it doesnt run
+
+
+
+let new_name="    saransh saurav     ";
+console.log(new_name);//    saransh saurav     -->prints with all unnecessary spaces
+console.log(new_name.trim());//saransh saurav-->remove satrting and ending spaces
+
+
+const url="https://saransh&32saurav.com"
+console.log(url.replace("&32","-"));//https://saransh-saurav.com --->replace element item
+//syntax replace("old element to be replaced","new element")
+
+console.log(url.includes("saransh"));//true -->tells if url contains the given word or not
+console.log(url.includes("poison"));//false
+
+
+
+const variable="saransh-saurav-mishra";
+console.log(variable.split("-"));//make string to an array on basi of seperator given by us(here seperator is "-")
+//one more example
+const words="he is a very good boy";
+arrayWords=words.split(" ");
+console.log(arrayWords);//[ 'he', 'is', 'a', 'very', 'good', 'boy' ] it is just as it was in python
+
 
 
 
