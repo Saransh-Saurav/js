56e4cdf4cfe13d2e3678966550d1687dfab5a2ee
Saransh-Saurav <saranshcollegebackup@gmail.com>
2025-07-23 03:06:20 +0000

    jh

--- 01-basics/05_date.js
+++ 01-basics/05_date.js
@@ -23,3 +23,16 @@ let mycreatedDate2=new Date("2023,01,03");//yyyy/mm/dd
 console.log(mycreatedDate2.toString());//Tue Jan 03 2023 00:00:00 GMT+0000 (Coordinated Universal Time)
 mycreatedDate2=new Date("01,07,2024");//mm/dd/yyyy
 console.log(mycreatedDate2.toString());//Sun Jan 07 2024 00:00:00 GMT+0000 (Coordinated Universal Time)
+
+
+
+
+let mytimestamp= Date.now();
+console.log(mytimestamp);//1753238160666 -->it gives time in millisecond and its reference is from 1st Jan 1970
+console.log(Math.floor(Date.now()/1000))//1753238700 -->we get the time in seconds by /1000 and by math.floor we ignored the decimal value that was in millisecond
+
+console.log(mycreatedDate.getTime());//1704429023000 -->convert date to time in millisecond eith reference to 1 Jan 1970 so that we can compare 2 dates easily
+
+
+let newDate= new Date();
+console.log( newDate.toLocaleString('default'));//string -->
