7e4946a14e3492850655b2e2fcc3e9eb001bca82
Saransh-Saurav <saranshcollegebackup@gmail.com>
2025-07-23 14:57:38 +0000

    done

--- 02-basics/01_array.js
+++ 02-basics/01_array.js
@@ -69,3 +69,52 @@ console.log("C",arr);//C [ 44, 3, 4, 5 ]-->array changed ->the section of index 
 
 
 
+
+const arr1=["green","blue","red"];
+const arr2=["pink","yellow","orange"];
+const arr3=[2,4,6,8];
+
+// arr1.push(arr2); //Appends new elements to the end of an array, and returns the new length of the array.
+// console.log(arr1); //[ 'green', 'blue', 'red', [ 'pink', 'yellow', 'orange' ] ]
+//notice that array 2 element dont gets appended to array 1 ,rather arr2 altogeather get appended to arr1 ie now arr1[3]= [ 'pink', 'yellow', 'orange' ]
+
+
+//METHOD 1 OF MERGINING 2 OR MORE ARRAYS ELEMENT WISE
+const arr4=arr1.concat(arr2,arr3);//Combines two or more arrays. This method returns a new array without modifying any existing arrays.
+console.log(arr4);//['green','blue','red','pink','yellow','orange', 2,4,6,8]
+
+//METHOD 2 OF MERGINING 2 OR MORE ARRAYS ELEMENT WISE
+const arr5=[...arr1,...arr2,...arr3];//['green','blue','red','pink','yellow','orange', 2,4,6,8]
+console.log(arr5);
+
+const arr6=[...arr1,arr2,arr3]//
+console.log(arr6);//['green','blue','red',[ 'pink', 'yellow', 'orange' ],[ 2, 4, 6, 8 ] ]
+// ...->adds the elements of array while array name adds whole array in new one 
+
+
+
+const array=[1,2,3,5,[3,4,6,[3,4,5,5]],3,5,7,[5,4,3,2,[4,4,3,2,[3,3,4,3]]]];
+const usable_array=array.flat(Infinity);//Returns a new array with all sub-array elements concatenated into it recursively up to the specified depth.
+// depth — The maximum recursion depth ,for ex in 'array' we have max depth 3,
+//  like max three additional array one inside other
+//we can set the depth to infinity so that we dont have to count the depth
+console.log(usable_array);//[1, 2, 3, 5, 3, 4, 6, 3,4, 5, 5, 3, 5, 7, 5, 4,3, 2, 4, 4, 3, 2, 3, 3,4, 3]
+
+
+
+console.log(Array.isArray("saransh"));//false
+console.log(Array.from("saransh"));//['s', 'a', 'r', 'a', 'n', 's','h']
+//Creates an array from an iterable object.
+
+// Array.from() converts any element to an array
+
+console.log(Array.from({myname:"Saransh",age:19}));//[]
+//gives an empty array bcz we didn't specify as if 'key' or 'value' should be made an array
+
+let score1=100;
+let score2=200;
+let score3=300;
+let scores=[123,234,678];
+
+console.log(Array.of(score1,score2,score3,...scores));//[ 100, 200, 300, 123, 234, 678 ]
+//Returns a new array from a set of elements.
